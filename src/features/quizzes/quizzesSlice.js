9c6d6f36ae57b227afd274b4daa5c2be6abe6ec6
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-03-30 17:06:25 +0100

    Create action creator that returns thunk so that we can add quizzes to quiz state at the same time as adding quiz ids to the topics state

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addQuiz } from "../topics/topicsSlice";
+import { useDispatch } from "react-redux";
 
 const quizzesSlice = createSlice({
   name: "quizzes",
@@ -17,6 +19,16 @@ const quizzesSlice = createSlice({
     }
   }
 });
+
+const dispatch = useDispatch();
+
+export const addQuizToQuizzesAndTopics = (quiz) => {
+  const { topicId, id } = quiz;
+  return (dispatch) => {
+    dispatch(quizzesSlice.actions.addQuiz(quiz));
+    dispatch(addQuizId({ quizId: id, topicId: topicId }));
+  };
+};
 
 export const selectQuizzes = (state) => state.quizzes.quizzes;
 export const { addQuiz } = quizzesSlice.actions;
--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -15,13 +15,13 @@ const topicsSlice = createSlice({
         quizIds: []
       };
     },
-    addQuiz: (state, action) => {
+    addQuizId: (state, action) => {
       const { quizId, topicId } = action.payload;
-      state.topics[topicId][quizIds].push(quizId);
+      state.topics[topicId].quizIds.push(quizId);
     }
   }
 });
 
 export const selectTopics = (state) => state.topics.topics;
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
