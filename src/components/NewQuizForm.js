3ff8726604337b14bf892bd5c5d1e2d2ba4cd09b
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-03-30 17:10:22 +0100

    Import topics selector into new quiz form and set up a topics variable using useSelector

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
-import ROUTES from "../app/routes";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import ROUTES from '../app/routes';
+import { selectTopics } from '../features/topics/topicsSlice';
 
 export default function NewQuizForm() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [cards, setCards] = useState([]);
-  const [topicId, setTopicId] = useState("");
+  const [topicId, setTopicId] = useState('');
   const history = useHistory();
-  const topics = {};
+  const topics = useSelector(selectTopics);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,7 +27,7 @@ export default function NewQuizForm() {
 
   const addCardInputs = (e) => {
     e.preventDefault();
-    setCards(cards.concat({ front: "", back: "" }));
+    setCards(cards.concat({ front: '', back: '' }));
   };
 
   const removeCard = (e, index) => {
@@ -45,17 +46,17 @@ export default function NewQuizForm() {
       <h1>Create a new quiz</h1>
       <form onSubmit={handleSubmit}>
         <input
-          id="quiz-name"
+          id='quiz-name'
           value={name}
           onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Quiz Title"
+          placeholder='Quiz Title'
         />
         <select
-          id="quiz-topic"
+          id='quiz-topic'
           onChange={(e) => setTopicId(e.currentTarget.value)}
-          placeholder="Topic"
+          placeholder='Topic'
         >
-          <option value="">Topic</option>
+          <option value=''>Topic</option>
           {Object.values(topics).map((topic) => (
             <option key={topic.id} value={topic.id}>
               {topic.name}
@@ -63,34 +64,34 @@ export default function NewQuizForm() {
           ))}
         </select>
         {cards.map((card, index) => (
-          <div key={index} className="card-front-back">
+          <div key={index} className='card-front-back'>
             <input
               id={`card-front-${index}`}
               value={cards[index].front}
               onChange={(e) =>
-                updateCardState(index, "front", e.currentTarget.value)
+                updateCardState(index, 'front', e.currentTarget.value)
               }
-              placeholder="Front"
+              placeholder='Front'
             />
 
             <input
               id={`card-back-${index}`}
               value={cards[index].back}
               onChange={(e) =>
-                updateCardState(index, "back", e.currentTarget.value)
+                updateCardState(index, 'back', e.currentTarget.value)
               }
-              placeholder="Back"
+              placeholder='Back'
             />
 
             <button
               onClick={(e) => removeCard(e, index)}
-              className="remove-card-button"
+              className='remove-card-button'
             >
               Remove Card
             </button>
           </div>
         ))}
-        <div className="actions-container">
+        <div className='actions-container'>
           <button onClick={addCardInputs}>Add a Card</button>
           <button>Create Quiz</button>
         </div>
